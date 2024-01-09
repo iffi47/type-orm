586f2e5f37385f44a282905243622f7584a598fc
iftikharSatusSystem <iftikhar.satussystems@gmail.com>
2024-01-09 12:58:15 +0500

    feat: add bootstrap func

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -4,17 +4,28 @@ import { Tweets } from "./entity/Tweet";
 
 export const Bootstrap = async () => {
   const userRepo = AppDataSource.getRepository(User);
+  console.log(userRepo, "this is user repository");
+
   const user = userRepo.create({
     firstName: "Alex",
     lastName: "Ali",
+    age: 45,
   });
-  await userRepo.save(user).catch((err) => {
+  console.log(user);
+  let userSave = await userRepo.save(user).catch((err) => {
     console.log(err);
   });
+  console.log(userSave);
+
   console.log("New user Saved", user);
 
   const tweetRepo = AppDataSource.getRepository(Tweets);
   const tweet = new Tweets();
   tweet.title = "PTI Hangouts";
   tweet.content = "asdafsdfsldklsdfjkajkfjsklkjf";
+  tweet.user = user;
+
+  await tweetRepo.save(tweet).catch((err) => {
+    console.log(err);
+  });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,32 @@
+import { Bootstrap } from "./bootstrap";
 import { AppDataSource } from "./data-source";
 import { User } from "./entity/User";
 
 AppDataSource.initialize()
   .then(async () => {
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    console.log(user);
-    console.log(User);
+    // console.log("Inserting a new user into the database...");
+    // const user = new User();
+    // user.firstName = "Timber";
+    // user.lastName = "Saw";
+    // user.age = 25;
+    // console.log(user);
+    // console.log(User);
 
-    const saveUser = await AppDataSource.manager.save(user);
-    console.log(saveUser);
-    console.log(user);
-    console.log("Saved a new user with id: " + user.id);
+    // const saveUser = await AppDataSource.manager.save(user);
+    // console.log(saveUser);
+    // console.log(user);
+    // console.log("Saved a new user with id: " + user.id);
 
-    console.log("Loading users from the database...");
-    const users = await AppDataSource.manager.find(User);
-    console.log("Loaded users: ", users);
+    // console.log("Loading users from the database...");
+    // const users = await AppDataSource.manager.find(User);
+    // console.log("Loaded users: ", users);
 
-    console.log(
-      "Here you can setup and run express / fastify / any other framework."
-    );
+    // console.log(
+    //   "Here you can setup and run express / fastify / any other framework."
+    // );
+
+    Bootstrap().catch((err) => {
+      console.log(err);
+    });
   })
   .catch((error) => console.log(error));
